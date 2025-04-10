2eebd051ce18250d65a181d75c37ab10f2ea78e9
Gopi Shankar Ravady <gopi2605@gmail.com>
2025-04-10 16:11:26 -0500

    server side push for Week 4 tasks

--- server/src/index.js
+++ server/src/index.js
@@ -1,20 +1,39 @@
-const app = require("express")();
-const server = require("http").createServer(app);
+const express = require("express");
+const http = require("http");
 const cors = require("cors");
-const io = require("socket.io")(server, {
-    cors: {
-        origin: "*",
-        methods: ["GET", "POST"]
-    }
+const socketIO = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
 });
 
 app.use(cors());
+
 const PORT = process.env.PORT || 8080;
+
 app.get('/', (req, res) => {
-        res.send('Hello World');
+  res.send('Hello World');
 });
 
 io.on("connection", (socket) => {
-    socket.emit("me", socket.id);
+  console.log(`Client connected: ${socket.id}`);
+
+  socket.emit("me", socket.id);
+
+  socket.on("offer", (offer) => {
+    console.log(`Offer received from ${socket.id}:`, offer);
+    
+    socket.broadcast.emit("offer", offer);
+  });
+
+  socket.on("disconnect", () => {
+    console.log(`Client disconnected: ${socket.id}`);
+  });
 });
+
 server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
